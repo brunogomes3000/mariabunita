72c3bda347a5aeeded8d32e534ff5b29db79b0f1
raimariae1 <raianericardosantos@gmail.com>
2017-11-29 11:32:41 -0300

    add conteudo de admin.py

--- mariabunita/core/admin.py
+++ mariabunita/core/admin.py
@@ -1,6 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Usuario
+from .models import Tipo_Usuario
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cpf']
+    search_fields = ['nome', 'cpf']
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Tipo_Usuario)
 
 # Register your models here.
