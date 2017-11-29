390e7f55d4a892c6f0f07c8b1029b4daf7c4352b
raimariae1 <raianericardosantos@gmail.com>
2017-11-29 11:27:01 -0300

    add conteudo de views.py

--- mariabunita/core/views.py
+++ mariabunita/core/views.py
@@ -1,10 +1,21 @@
-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Usuario
 
-def index (request)
-return render(request, 'index.html')
+def index(request):
+    return render(request,'index.html')
+def contato(request):
+    return render (request, 'contato.html')
+def sobre(request):
+    return render (request, 'sobre.html')
+def usuarios(request):
+    usuarios = Usuario.objects.all()
+    context = {
+    'usuarios': usuarios
+    }
+    return render(request, 'usuarios.html', context)
+# Create your views here.
 
-def index(request)
-return render(request, 'cadastro.html')
+
+def login (resquest):
+	return render (resquest, 'login.html')
