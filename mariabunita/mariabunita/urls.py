7589e60027f3a9864ed5b5ca9298f1ee6a067315
raimariae1 <raianericardosantos@gmail.com>
2017-12-04 17:20:06 -0300

    login.html inseridos codigos

--- mariabunita/mariabunita/urls.py
+++ mariabunita/mariabunita/urls.py
@@ -3,11 +3,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from core import views 
 urlpatterns = [
-
-	url (r'^sac/$', views.sac, name="sac"), 
-
-	url(r'cadastro/$', views.cadastro,name="cadastro"),
-
+	url(r'^', views.index, name="index"), 
+	url(r'^sac/$', views.sac, name="sac"), 
+	url(r'^cadastro/$', views.cadastro,name="cadastro"),
     url(r'^admin/', admin.site.urls),
 
 ]
