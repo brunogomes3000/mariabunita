4cf7add524a38978fe4c210aabfe1a3568f68866
nomeiodabia <biatargino262@gmail.com>
2017-12-04 17:42:58 -0300

    atualizei urls.py

--- mariabunita/mariabunita/urls.py
+++ mariabunita/mariabunita/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 from core import views 
 urlpatterns = [
 	url(r'^', views.index, name="index"), 
-	url(r'^sac/$', views.sac, name="sac"), 
+	url(r'^sac/$', views.sac, name="sac"),
+	url(r'^visuvendas/$', views.visuvendas, name="visuvendas"), 
 	url(r'^cadastro/$', views.cadastro,name="cadastro"),
     url(r'^admin/', admin.site.urls),
 
